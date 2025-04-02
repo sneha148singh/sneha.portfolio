f87afc487a0bcf25a61c9ebd3facb148abbd05bb
Sneha_148 <snehasingh10921@gmail.com>
2025-04-02 23:09:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onlaod=function(){
+    alert("welcome to my portfolio!\n after entering click the main heading:");
+};
+let heading=document.getElementByid("heading");
+heading.addEventListener("click function"){
+    heading.textcontent="This Portfolio is made by Sneha Singh";
+}
